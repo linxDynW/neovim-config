92dc0ad894a7c6cf48c642eb025393aede4165bd
linxDynW <2500137889@qq.com>
2026-05-14 21:06:19 +0800

    use the catppuccin mocha

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,54 +1,83 @@
 return {
     {
-        "ellisonleao/gruvbox.nvim",
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
-        config = function()
-            require("gruvbox").setup({
-                terminal_colors = true,
-                undercurl = true,
-                underline = true,
-                bold = true,
-                italic = {
-                    strings = true,
-                    emphasis = true,
-                    comments = true,
-                    operators = false,
-                    folds = true,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true,
-                contrast = "hard",
-                palette_overrides = {},
-                overrides = {
-                    -- Normal = { bg = "none" },
-                    Normal = { fg = "#fbf1c7" },
-
-                    -- ["@keyword"] = { fg = "#fe8019", bold = true },
-                    -- ["@type"] = { fg = "#fabd2f", bold = true },
-                    -- ["@function"] = { fg = "#b8bb26", bold = true },
-                    -- ["@variable"] = { fg = "#ebdbb2" },
-                    -- ["@property"] = { fg = "#83a598" },
-                    -- ["@string"] = { fg = "#b8bb26" },
-                    -- ["@constant"] = { fg = "#d3869b" },
-                    --
-                    -- ["@asm"] = { fg = "#fb4934" },
-
+        opts = {
+            transparent_background = true,
+            custom_highlights = function(colors)
+                return {
                     NormalFloat = { bg = "none" },
                     FloatBorder = { bg = "none" },
-                    SignColumn = { bg = "none" },
-                    EndOfBuffer = { bg = "none" },
-                    Folded = { bg = "none" },
-                    FoldColumn = { bg = "none" },
+                    NeoTreeNormal = { bg = "none" },
+                    NeoTreeNormalNC = { bg = "none" },
+                    TelescopeNormal = { bg = "none" },
+                    TelescopeBorder = { bg = "none" },
+                    TelescopeTitle = { bg = "none" },
+                    SnacksNormal = { bg = "none" },
+                    SnacksNormalNC = { bg = "none" },
+
+                    -- BufferLineFill = { bg = colors.crust },
+                    -- BufferLineBackground = { bg = colors.crust },
+                    -- BufferLineSeparator = { fg = colors.crust, bg = colors.crust },
+                    -- BufferLineOffsetSeparator = { fg = colors.crust, bg = colors.crust },
+
                     CursorLine = { bg = "none", underline = true },
-                },
-                transparent_mode = true,
-            })
-            vim.cmd("colorscheme gruvbox")
-        end,
+                    CursorLineNr = { fg = colors.mauve, bg = "none", style = { "bold" } },
+
+                    WinBar = { bg = "none" },
+                    WinBarNC = { bg = "none" },
+                    SignColumn = { bg = "none" },
+                    FoldColumn = { bg = "none" },
+                }
+            end,
+        },
     },
-    -- { "folke/tokyonight.nvim", enabled = false },
+    -- 确保主题被正确应用
+    -- {
+    --     "catppuccin/nvim",
+    --     name = "catppuccin",
+    --     opts = {
+    --         transparent_background = true,
+    --         show_end_of_buffer = false,
+    --         integration_default = false,
+    --         integrations = {
+    --             -- 开启这些插件的透明集成
+    --             cmp = true,
+    --             gitsigns = true,
+    --             nvimtree = true,
+    --             treesitter = true,
+    --             notify = true,
+    --             mini = {
+    --                 enabled = true,
+    --                 indentscope_color = "rosewater",
+    --             },
+    --             -- 重点：解决你图中标签页黑块的问题
+    --             bufferline = true,
+    --             native_lsp = {
+    --                 enabled = true,
+    --                 virtual_text = {
+    --                     errors = { "italic" },
+    --                     hints = { "italic" },
+    --                     warnings = { "italic" },
+    --                     information = { "italic" },
+    --                 },
+    --             },
+    --         },
+    --         custom_highlights = function(colors)
+    --             return {
+    --                 -- 强制暴力清除你图中那些顽固的背景块
+    --                 BufferLineFill = { bg = "NONE" },
+    --                 BufferLineBackground = { bg = "NONE" },
+    --                 NeoTreeNormal = { bg = "NONE" },
+    --                 NeoTreeNormalNC = { bg = "NONE" },
+    --                 NeoTreeTabInactive = { bg = "NONE" },
+    --                 NeoTreeTabActive = { bg = "NONE" },
+    --                 NormalFloat = { bg = "NONE" }, -- 浮窗背景透明
+    --                 FloatBorder = { bg = "NONE" }, -- 浮窗边框透明
+    --             }
+    --         end,
+    --     },
+    -- },
+    { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-mocha" } },
 }
