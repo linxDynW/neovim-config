be2a99db64816407ef15123ee646b188b159472f
linxDynW <2500137889@qq.com>
2026-05-15 18:44:45 +0800

    add keys.lua

---
+++ lua/snippets/keys.lua
@@ -0,0 +1,11 @@
+vim.keymap.set("n", "<leader>t", function()
+    vim.api.nvim_put({ "\\n\\t" }, "c", true, true)
+end, { desc = 'Insert "\\n\\t"' })
+
+vim.keymap.set("n", "<A-S-h>", function()
+    vim.cmd("BufferLineMovePrev")
+end, { desc = "Move buffer to prev" })
+
+vim.keymap.set("n", "<A-S-l>", function()
+    vim.cmd("BufferLineMoveNext")
+end, { desc = "Move buffer to next" })
